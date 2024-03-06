8f56ca57d3b120d2dbf86c1998b841fe0a80c021
Rajat Kumar (CW) <Rajat.Kumar1@jda.com>
2024-03-06 15:24:21 +0530

    hey

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import{
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate
+  Navigate,
+  Link
 } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/about';
